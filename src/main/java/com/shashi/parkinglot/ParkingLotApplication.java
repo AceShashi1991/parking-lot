b0f811a4e384e446e054c46f81776ceb9e1cb2b9
AceShashi1991 <shashi52.699@gmail.com>
2023-12-15 18:06:17 +0530

    cehcking

--- src/main/java/com/shashi/parkinglot/ParkingLotApplication.java
+++ src/main/java/com/shashi/parkinglot/ParkingLotApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ParkingLotApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ParkingLotApplication.class, args);
     }
